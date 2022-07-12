167b6087cab841abf8d19af0f4b08c8b312f4194
salima00s2248 <s.shikhlarova@gmail.com>
2022-07-12 17:29:08 +0400

    School Management System created

---
+++ school-system/src/test/java/az/developia/schoolsystem/SchoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.schoolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
